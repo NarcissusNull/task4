2716186f8260ad74db873901b3d9832a0ad3f9d0
Narcissus <1169714441@qq.com>
2019-05-22 17:46:15 +0800

    完成GameControllerTest

--- src/test/java/tw/controllers/GameControllerTest.java
+++ src/test/java/tw/controllers/GameControllerTest.java
@@ -1,6 +1,18 @@
 package tw.controllers;
 
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import org.junit.Test;
+import tw.core.Game;
+import tw.core.exception.OutOfRangeAnswerException;
+import tw.core.generator.AnswerGenerator;
+import tw.core.generator.RandomIntGenerator;
+import tw.views.GameView;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * 在GameControllerTest文件中完成GameController中对应的单元测试
@@ -8,10 +20,16 @@ import org.junit.Test;
 public class GameControllerTest {
 
     @Test
-    public void beginGame() {
+    public void beginGame() throws OutOfRangeAnswerException, IOException {
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+        AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
+        Game game = new Game(answerGenerator);
+        GameView gameView = new GameView();
+        GameController gameController = new GameController(game, gameView);
+        ByteOutputStream out = new ByteOutputStream();
+        System.setOut(new PrintStream(out));
+        gameController.beginGame();
+        assertEquals(out.toString(), "------Guess Number Game, You have 6 chances to guess!  ------\r\n");
     }
 
-    @Test
-    public void play() {
-    }
 }
--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -2,6 +2,13 @@ package tw.core;
 
 
 import org.junit.Test;
+import tw.core.generator.RandomIntGenerator;
+
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
@@ -10,5 +17,9 @@ public class RandomIntGeneratorTest {
 
     @Test
     public void generateNums() {
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+        Random ran = mock(Random.class);
+        when(ran.nextInt(9)).thenReturn(1).thenReturn(2).thenReturn(3).thenReturn(4);
+        assertEquals(randomIntGenerator.generateNums(9,4), "1 2 3 4");
     }
 }
