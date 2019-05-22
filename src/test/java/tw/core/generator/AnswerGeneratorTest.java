176c4d7008ea8867a330955d663993ecc67935b5
Narcissus <1169714441@qq.com>
2019-05-22 17:03:49 +0800

    完成AnswerGeneratorTest

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,24 @@
 package tw.core.generator;
 
+import org.junit.Test;
+import tw.core.Answer;
+import tw.core.exception.OutOfRangeAnswerException;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
 
+    @Test
+    public void generate() throws OutOfRangeAnswerException {
+        RandomIntGenerator randomIntGenerator = mock(RandomIntGenerator.class);
+        when(randomIntGenerator.generateNums(9,4)).thenReturn("1 2 3 4");
+        AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
+
+        assertEquals(answerGenerator.generate().toString(), Answer.createAnswer("1 2 3 4").toString());
+    }
 }
 
