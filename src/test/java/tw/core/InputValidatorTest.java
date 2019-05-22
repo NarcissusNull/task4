0a91595714e57725fb3a4e4b9bc9ee07b7396cfc
Narcissus <1169714441@qq.com>
2019-05-22 17:21:25 +0800

    完成InputValidatorTest

--- src/test/java/tw/core/InputValidatorTest.java
+++ src/test/java/tw/core/InputValidatorTest.java
@@ -1,6 +1,9 @@
 package tw.core;
 
 import org.junit.Test;
+import tw.validator.InputValidator;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * 在InputValidatorTest文件中完成InputValidator中对应的单元测试
@@ -9,9 +12,10 @@ public class InputValidatorTest {
 
     @Test
     public void validate() {
+        InputValidator inputValidator = new InputValidator();
+        assertEquals(inputValidator.validate("1 2"), false);
+        assertEquals(inputValidator.validate("1 2 34 3"), false);
+        assertEquals(inputValidator.validate("1 2 3 4"), true);
     }
 
-    @Test
-    public void validate1() {
-    }
 }
